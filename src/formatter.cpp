0966e1d9473df42936606ec0ecda1f74629de66a
Wang Hetong <wanghetong@proton.me>
2026-01-14 18:26:04 +0800

    previous and next month's date greyed out

--- src/formatter.cpp
+++ src/formatter.cpp
@@ -4,8 +4,8 @@
 #include <termcolor/termcolor.hpp>
 
 void print_month(int month, int year) {
-  std::cout << "M  T  W  T  F  " << termcolor::red << "S  S" << termcolor::reset
-            << std::endl;
+  std::cout << "M  T  W  T  F  " << termcolor::bright_red << "S  S"
+            << termcolor::reset << std::endl;
 
   int last_month_day_count = get_daycount(month - 1);
   if (month == 1) {
@@ -16,14 +16,18 @@ void print_month(int month, int year) {
 
   int last_month_day = last_month_day_count - starting_weekday + 1;
 
+  // the previous month's dates
+  std::cout << termcolor::bright_grey;
   for (int i = 1; i <= starting_weekday; i++) {
     std::cout << last_month_day << " ";
     last_month_day++;
   }
+  std::cout << termcolor::reset;
 
+  // this month's dates
   for (int i = 1; i <= get_daycount(month); i++) {
     if (is_holiday(i, month, year)) {
-      std::cout << termcolor::red;
+      std::cout << termcolor::bright_red;
     }
 
     if (1 <= i && i <= 9) {
@@ -38,20 +42,20 @@ void print_month(int month, int year) {
     std::cout << termcolor::reset;
   }
 
+  // the next month's dates
   int next_month_start_count =
       7 - get_weekday(get_daycount(month), month, year) - 1;
 
   int next_month = month + 1;
   int target_year = year;
+  // in december we do it for the next year
   if (next_month == 13) {
     next_month = 1;
     target_year++;
   }
+  std::cout << termcolor::bright_grey;
   for (int i = 1; i <= next_month_start_count; i++) {
-    if (is_holiday(i, next_month, target_year)) {
-      std::cout << termcolor::red;
-    }
     std::cout << i << "  ";
-    std::cout << termcolor::reset;
   }
+  std::cout << termcolor::reset;
 }
