550774cdb3ae6b640f1094b2e438af0d6c0aaf43
Wang Hetong <wanghetong@proton.me>
2026-01-14 18:49:01 +0800

    highlight today in blue

--- src/formatter.cpp
+++ src/formatter.cpp
@@ -1,5 +1,6 @@
 #include "formatter.hpp"
 #include "date_util.hpp"
+#include <chrono>
 #include <iostream>
 #include <termcolor/termcolor.hpp>
 
@@ -24,22 +25,40 @@ void print_month(int month, int year) {
   }
   std::cout << termcolor::reset;
 
+  auto now = std::chrono::zoned_time{std::chrono::current_zone(),
+                                     std::chrono::system_clock::now()}
+                 .get_local_time();
+
+  std::chrono::year_month_day current_date{
+      std::chrono::floor<std::chrono::days>(now)};
+
+  unsigned int today = static_cast<unsigned int>(current_date.day());
+  unsigned int this_month = static_cast<unsigned int>(current_date.month());
+  int this_year = static_cast<int>(current_date.year());
+
   // this month's dates
   for (int i = 1; i <= get_daycount(month); i++) {
     if (is_holiday(i, month, year)) {
       std::cout << termcolor::bright_red;
     }
+    // is it today
+    if (static_cast<int>(today) == i && month == static_cast<int>(this_month) &&
+        year == this_year) {
+      std::cout << termcolor::on_blue;
+    }
+
+    std::cout << i;
+    std::cout << termcolor::reset;
 
     if (1 <= i && i <= 9) {
-      std::cout << i << "  ";
+      std::cout << "  ";
     } else {
-      std::cout << i << " ";
+      std::cout << " ";
     }
 
     if (get_weekday(i, month, year) == 6) {
       std::cout << std::endl;
     }
-    std::cout << termcolor::reset;
   }
 
   // the next month's dates
@@ -47,11 +66,9 @@ void print_month(int month, int year) {
       7 - get_weekday(get_daycount(month), month, year) - 1;
 
   int next_month = month + 1;
-  int target_year = year;
   // in december we do it for the next year
   if (next_month == 13) {
     next_month = 1;
-    target_year++;
   }
   std::cout << termcolor::bright_grey;
   for (int i = 1; i <= next_month_start_count; i++) {
